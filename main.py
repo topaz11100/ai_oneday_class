b2d24472c45be8dad91ae7d49cafe65014dd4502
topaz11100 <topaz11100@gmail.com>
2024-08-21 20:44:17 +0900

    시작

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import openaiapi
 
 st.title('제품 홍보 포스터 생성기')
 
-text_promft = "를 팔아야하는 세일즈맨이라고 생각하며 300바이트 이내의 솔깃한 제품 홍보 문구를 작성해줘."
+text_promft = "를 팔아야하는 세일즈맨이라고 생각하며 300바이트 이내의 솔깃한 제품 홍보 문구를 작성해줘. 줄바꿈해서 출력해줘"
 
 keyword = st.text_input("키워드 입력",
                         label_visibility='hidden',
